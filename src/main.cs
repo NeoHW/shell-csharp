eb01ba04774443c367b0724c932849248f66f1c2
NeoHW <neohaowei@gmail.com>
2025-01-04 14:57:03 +0800

    Invalid command check

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
 Console.Write("$ ");
 
 // Wait for user input
-Console.ReadLine();
+String userInputCommand = Console.ReadLine();
+Console.WriteLine($"{userInputCommand}, command not found");
