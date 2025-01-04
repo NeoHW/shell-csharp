98231f053a7ab0fc24235203716004d7ec481652
NeoHW <neohaowei@gmail.com>
2025-01-04 23:46:45 +0800

    Echo builtin

--- src/main.cs
+++ src/main.cs
@@ -1,14 +1,18 @@
+using System.Globalization;
 using System.Net;
 using System.Net.Sockets;
 
 namespace CommandParser {
     public class CommandParser {
-        private const string ExitCommand = "exit 0";
+        private const string ExitCommand = "exit";
+        private const string EchoCommand = "echo";
+        private const int ExitCode = 0;
  
         public void Run() {
             while (true) {
                 PrintUserInputLine();
-                HandleUserInput(); 
+                var userInput = Console.ReadLine();
+                HandleUserInput(userInput); 
             }
         }
 
@@ -16,16 +20,43 @@ namespace CommandParser {
             Console.Write("$ ");
         }
 
-        private void HandleUserInput() {
-            var userInputCommand = Console.ReadLine();
-            Console.WriteLine(ParseCommand(userInputCommand));
+        private void HandleUserInput(string? userInput) {
+            if (string.IsNullOrWhiteSpace(userInput)) {
+                Console.WriteLine("No command entered. Please try again.");
+                return;
+            }
+            
+            var (commandWord, args) = ExtractCommandAndArgs(userInput);
+            ExecuteCommand(commandWord, args);
         }
         
-        private string ParseCommand(string? command) {
-            if (command == ExitCommand) {
-                Environment.Exit(0);
-            }
-            return $"{command}: command not found";
+        private (string commandWord, string args) ExtractCommandAndArgs(string command) {
+            var parts = command.Split(" ", 2);
+            var commandWord = parts.Length > 0 ? parts[0] : string.Empty;
+            var args = parts.Length > 1 ? parts[1] : string.Empty;
+            return (commandWord, args);
+        }
+        
+        private void ExecuteCommand(string commandWord, string args) {
+           switch (commandWord.ToLowerInvariant()) {
+               case ExitCommand:
+                   ExitApplication();
+                   break;
+               case EchoCommand:
+                   ExecuteEchoCommand(args);
+                   break;
+               default:
+                   Console.WriteLine($"{commandWord}: command not found");
+                   break;
+           }
+        }
+
+        private static void ExitApplication() {
+            Environment.Exit(ExitCode);
+        }
+
+        private void ExecuteEchoCommand(string args) {
+            Console.WriteLine(args);
         }
     }
 
